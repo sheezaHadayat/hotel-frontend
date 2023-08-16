a10ffe6b0b24c7a71532ea4cb76c6774421f3cd3
sheezaHadayat <sheezahadayat@gmail.com>
2023-08-16 11:14:06 +0500

    end

--- src/screens/landingScreen.jsx
+++ src/screens/landingScreen.jsx
@@ -6,8 +6,8 @@ export default function LandingScreen() {
     <div className='row landing my-auto justify-content-center' >
         <div className="col-md-9 my-auto text-center" style={{borderRight:'8px solid white'}}>
             <h2 className='lnh2' style={{color:'white',fontWeight:'bold' }}>Hotel Booking</h2>
-            <h3 className="lnh3" style={{color:'white', marginTop:'50px'}}>There is only one boss. </h3>
-            <Link to ="/home">
+            <h3 className="lnh3" style={{color:'white', marginTop:'50px'}}>Taking luxury to new levels </h3>
+            <Link to ="/login">
             <button className='btn' style={{backgroundColor:'white', color:'black', marginTop:'20px'}}>Get Started </button> 
             </Link>
         </div>
