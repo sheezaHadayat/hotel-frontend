5586b2db03d79bd7460ea51dd36691438a83cee8
sheezaHadayat <sheezahadayat@gmail.com>
2023-08-17 00:14:22 +0500

    updated

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
     <div>
       <nav class="navbar navbar-expand-lg ">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/home" style={{ color: 'white' }}>Hotel</a>
+          <a class="navbar-brand" href="#" style={{ color: 'white' }}>Hotel</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"><i class='fa fa-bars' style={{color:'white'}}> </i></span>
           </button>
